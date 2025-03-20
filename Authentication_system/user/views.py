7b3a9b8235bc265a6664ba1d57e350d1722b401b
shakib071 <shakibhasan071@gmail.com>
2025-03-20 22:27:39 +0600

    added signup

--- Authentication_system/user/views.py
+++ Authentication_system/user/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .froms import UserRegistrationForm
+from django.contrib import messages
 
 # Create your views here.
+
+def user_signUp(request):
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"SignUp Successful")
+            return redirect('homepage')
+    else:
+        form = UserRegistrationForm()
+            
+    return render(request,'signup.html',{'form':form})
