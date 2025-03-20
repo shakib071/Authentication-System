815d4183d47817a00e4a55467f8628f95020a215
shakib071 <shakibhasan071@gmail.com>
2025-03-20 22:52:33 +0600

    added login

--- Authentication_system/user/urls.py
+++ Authentication_system/user/urls.py
@@ -2,4 +2,5 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
     path('signUp/', views.user_signUp,name='signUp'),
+    path('login/', views.user_login,name='login'),
 ]
--- Authentication_system/user/views.py
+++ Authentication_system/user/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 from .froms import UserRegistrationForm
 from django.contrib import messages
+from django.contrib.auth import login,logout,authenticate
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 
@@ -15,3 +17,21 @@ def user_signUp(request):
         form = UserRegistrationForm()
             
     return render(request,'signup.html',{'form':form})
+
+def user_login(request):
+    # user_data = request.POST
+    if request.method == "POST":
+        form = AuthenticationForm(request,request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request,username = username, password = password)
+
+            if user is not None:
+                login(request, user)
+                messages.success(request,"Logged in Successfully")
+                return redirect('homepage')
+    else:
+        form = AuthenticationForm()
+
+    return render(request,'login.html',{'form':form})
